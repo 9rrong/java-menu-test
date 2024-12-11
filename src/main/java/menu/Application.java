d9d5328ad8a92e206e07f4cfc757c72c3d2da5ab
goohong <goohong.chung@gmail.com>
2024-12-12 00:38:07 +0900

    feat(Application): 기초 MVC 틀 생성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,24 @@
 package menu;
 
+import menu.controller.MenuController;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
+    }
+
+    private void run() {
+        MenuController menuController = initializeComponents();
+        menuController.run();
+    }
+
+    private MenuController initializeComponents() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        return new MenuController(inputView, outputView);
     }
 }
---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,7 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+}
